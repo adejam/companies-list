a4a78762be52dc0ce1c6fe3b0628eeb9517f4741
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-05-24 19:22:57 +0100

    chore: add redux store config

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createWrapper } from 'next-redux-wrapper';
+import rootReducer from './reducers/index';
+
+const middleware = [thunk];
+const store = () => createStore(rootReducer, compose(applyMiddleware(...middleware)));
+
+const wrapper = createWrapper(store);
+
+export default wrapper;
