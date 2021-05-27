7b5ebcf5cb049af5074f69ff283637fbf135ea44
Adejam <oluwaloseyifunmiadeleye1@gmail.com>
2021-05-27 23:38:15 +0100

    chore: update server configurations to use real webiste domain

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com';
+const server = dev ? 'http://localhost:3000' : 'https://companies-list.vercel.app';
 
 export default server;
